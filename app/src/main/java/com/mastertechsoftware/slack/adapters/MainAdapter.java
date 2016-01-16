7fac0d6c958faded28260e7687c5af0186c8820c
Kevin Moore <kevindavmoore@gmail.com>
2016-01-16 14:16:04 -0800

    Add ImageView to main adapter

--- app/src/main/java/com/mastertechsoftware/slack/adapters/MainAdapter.java
+++ app/src/main/java/com/mastertechsoftware/slack/adapters/MainAdapter.java
@@ -5,12 +5,16 @@ package com.mastertechsoftware.slack.adapters;
  */
 
 import com.mastertechsoftware.slack.R;
+import com.mastertechsoftware.slack.models.Profile;
 import com.mastertechsoftware.slack.models.User;
+import com.squareup.picasso.Picasso;
 
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -36,8 +40,25 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.UserViewHolder
 	public void onBindViewHolder(UserViewHolder holder, int position) {
 		final User user = users.get(position);
 		holder.username.setText(user.getName());
+		Picasso.with(holder.username.getContext()).load(getProfileImage(user.getProfile())).into(holder.userImage);
 	}
 
+	/**
+	 * Try to load the largest image possible for the best results
+	 * @param profile
+	 * @return image url
+	 */
+	private String getProfileImage(Profile profile) {
+		if (!TextUtils.isEmpty(profile.getImage_72())) {
+			return profile.getImage_72();
+		} else if (!TextUtils.isEmpty(profile.getImage_48())) {
+			return profile.getImage_48();
+		} else if (!TextUtils.isEmpty(profile.getImage_32())) {
+			return profile.getImage_32();
+		} else  {
+			return profile.getImage_24();
+		}
+	}
 
 	@Override
 	public int getItemCount() {
@@ -57,6 +78,7 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.UserViewHolder
 	 */
 	public class UserViewHolder extends RecyclerView.ViewHolder {
 		protected TextView username;
+		protected ImageView userImage;
 		public UserViewHolder(View itemView) {
 			super(itemView);
 			if (onClickListener != null) {
@@ -65,6 +87,7 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.UserViewHolder
 			// This helps us find the holder when the view is clicked on
 			itemView.setTag(this);
 			username = (TextView) itemView.findViewById(R.id.username);
+			userImage = (ImageView) itemView.findViewById(R.id.userImage);
 		}
 	}
 
